23fbb11398d8c482fb2cd7df362e511bc96ac0b7
Thaila Esteves <thailalestevesl@gmail.com>
2024-09-03 15:01:06 -0300

    fixing show event

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -4,4 +4,27 @@ class TicketsController < ApplicationController
   def index
     @tickets = Ticket.where(user_id: current_user.id)
   end
+
+  def new
+    @event = Event.find(params[:event_id])
+    @ticket = Ticket.new
+  end
+
+  def create
+    @event = Event.find(params[:event_id])
+    @ticket = Ticket.new(ticket_params)
+    @ticket.event = @event
+    @ticket.user = current_user
+    if @ticket.save
+      redirect_to tickets_path
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def ticket_params
+    params.require(:ticket).permit(:id)
+  end
 end
