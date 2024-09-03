29e3f8f83d89f11a13d1b0af59e3744b6bc87838
paulo mielmiczuk <mielmiczukpaulo@gmail.com>
2024-09-03 11:10:58 -0300

    Alterações de front na Navbar e configuração do Search

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,11 @@ class Event < ApplicationRecord
   has_many :tickets, dependent: :destroy
 
   has_one_attached :photo
+
+  validates :title, :location, :description, :user, presence: true
+  validates :name, uniqueness: true
+
+  include PgSearch::Model
+  pg_search_scope :search_by_title_and_location,
+  against: %I[title location], using: { tsearch: { prefix: true } }
 end
