aa2871660d43c02b28e491c2f4860c0844c1778d
Tomas Vannucchi <tomas.vannucchi@gmail.com>
2024-09-05 15:34:46 -0300

    Buy button modal

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
   resources :events do
     resources :tickets, only: %i[new create destroy]
-    resources :orders, only: %i[new create]
+    resources :orders, only: %i[create]
   end
 
   resources :tickets, only: %i[index]
