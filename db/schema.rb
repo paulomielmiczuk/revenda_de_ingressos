436e1df03e8dd30d5ce453b0c3908d3d1d4b08d2
Tomas Vannucchi <tomas.vannucchi@gmail.com>
2024-09-10 09:17:45 -0300

    forum

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_09_191838) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_09_170254) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -95,7 +95,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_09_191838) do
     t.string "ticket_type"
     t.boolean "available"
     t.integer "price_cents", default: 0, null: false
-    t.string "image"
     t.index ["event_id"], name: "index_tickets_on_event_id"
     t.index ["user_id"], name: "index_tickets_on_user_id"
   end
